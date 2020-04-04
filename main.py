4a0ef3adec7cf8176b79bf9c9c5105799abe8b04
Emil <emil30606@gmail.com>
2020-04-04 20:42:52 +0300

    good

--- main.py
+++ main.py
@@ -1,65 +1,99 @@
 import sys
 
-from PyQt5 import uic, QtGui
+from PyQt5 import uic
 from PyQt5.QtCore import QTimer, QPropertyAnimation, QSize
-from PyQt5.QtGui import QMovie, QPixmap, QPainter
-from PyQt5.QtWidgets import *
-import sys
 from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 ######################################################################################################
+far = False
+cel = False
 
-class MyDialog (QMainWindow) :
+
+class MyDialog(QMainWindow) :
+
   def __init__(self) :
-    super ().__init__ ()
-    uic.loadUi ("test.ui", self)
+    super().__init__()
+    self.animations = []
+    uic.loadUi("test.ui", self)
     movie = QMovie("load.gif")
     self.myLabel.setMovie(movie)
     movie.start()
-    self.setStyleSheet ("QMainWindow {background: 'white';}")
-    self.pushButton.clicked.connect (self.buttonPress)
-
+    self.setStyleSheet("QMainWindow {background: 'white';}")
+    self.pushButton.clicked.connect(self.buttonPress)
     self.pushButton.hide()
     self.lineEdit.hide()
 
-    self.pushButton.setIconSize (QSize (1000, 1500))
+    self.celciusAction.triggered.connect(self.celcius_menu_action)
+    self.fahrenheitAction.triggered.connect(self.faringate_menu_action)
+
+    self.pushButton.setIconSize(QSize(1000, 1500))
     QTimer.singleShot(4000,
-                      lambda: self.doneLoading())
+                      lambda : self.doneLoading())
 
   def fade(self, widget) :
-    self.effect = QGraphicsOpacityEffect ()
-    widget.setGraphicsEffect (self.effect)
+    self.effect = QGraphicsOpacityEffect()
+    widget.setGraphicsEffect(self.effect)
 
-    self.animation = QPropertyAnimation (self.effect, b"opacity")
-    self.animation.setDuration (1000)
-    self.animation.setStartValue (1)
-    self.animation.setEndValue (0)
-    self.animation.start ()
+    animation = QPropertyAnimation(self.effect, b"opacity")
+    animation.setDuration(1000)
+    animation.setStartValue(1)
+    animation.setEndValue(0)
+    animation.start()
+    self.animations.append(animation)
 
   def unfade(self, widget) :
-    self.effect = QGraphicsOpacityEffect ()
-    widget.setGraphicsEffect (self.effect)
-    self.animation = QPropertyAnimation (self.effect, b"opacity")
-    self.animation.setDuration (3000)
-    self.animation.setStartValue (0)
-    self.animation.setEndValue (1)
-    self.animation.start ()
-
-  def doneLoading(self):
-    self.pushButton.show()
-    self.myLabel.hide()
-    self.lineEdit.show()
-    self.unfade (self.pushButton)
+    self.effect = QGraphicsOpacityEffect()
+    widget.setGraphicsEffect(self.effect)
+    animation = QPropertyAnimation(self.effect, b"opacity")
+    animation.setDuration(3000)
+    animation.setStartValue(0)
+    animation.setEndValue(1)
+    animation.start()
+    self.animations.append(animation)
 
   def buttonPress(self) :
-    print ("pressed!" + self.lineEdit.text())
+    input = self.lineEdit.text()
+    if (far) :
+      Fahrenheit = (input * 9 / 5) + 32
+      back = "weather"
+
     self.myLabel.hide()
 
+  def unf(self, widget) :
+    self.effect = QGraphicsOpacityEffect()
+    widget.setGraphicsEffect(self.effect)
+    self.animation = QPropertyAnimation(self.effect, b"opacity")
+    self.animation.setDuration(3000)
+    self.animation.setStartValue(0)
+    self.animation.setEndValue(1)
+    self.animation.start()
 
-if __name__=='__main__':
-    app = QApplication (sys.argv)
-    dialog = MyDialog ()
-    dialog.show ()
-    sys.exit (app.exec_ ())
+  def doneLoading(self) :
+    self.pushButton.show()
+    self.setStyleSheet("QMainWindow {background: rgba(247, 247, 239 ,255)}")
+    self.setStyleSheet("QMainWindow {background: hsl(60, 33, 95)}")
+    self.myLabel.hide()
+    self.lineEdit.show()
+    self.unfade(self.pushButton)
+    self.unfade(self.lineEdit)
+    self.setStyleSheet("QLineEdit { border-radius: 5px; }")
 
 
+
+
+  def faringate_menu_action(self) :
+    far = True
+    self.lineEdit.setText("°")
+    # self.lbl.setText(self.qle.text()) label from input
+
+  def celcius_menu_action(self) :
+    cel = True
+    self.lineEdit.setText("°")
+
+
+if __name__ == '__main__' :
+  app = QApplication(sys.argv)
+  dialog = MyDialog()
+  dialog.show()
+  sys.exit(app.exec_())
